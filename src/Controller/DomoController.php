641030c776e8069304219a4737e96dc9f5666f08
Mataoanu Alex <alexmatza01>
2018-04-02 13:42:45 +0300

    C`mon

--- src/Controller/DomoController.php
+++ src/Controller/DomoController.php
@@ -8,8 +8,8 @@
 
 namespace App\Controller;
 
-use App\Form\Type\DomoType;
-use App\Entity\Domo;
+use Interactions\FormBundle\Form\Type\DomoType;
+use Interactions\FormBundle\Entity\Domo;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
